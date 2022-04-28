a8ef50b1e40834a1c1f928ca4cf62e332ecd8ab6
Eduardo Pereira <eduardo.pereira@aiqfome.com>
2022-04-28 10:04:35 -0300

    feat:Main and User Activitys

---
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -0,0 +1,22 @@
+package com.example.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+    }
+
+    override fun onClick(view: View) {
+        TODO("Not yet implemented")
+    }
+}
