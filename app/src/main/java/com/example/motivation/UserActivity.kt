c95746811f25d075c430e0fa41b3cf218df484fc
Eduardo Pereira <eduardo.pereira@aiqfome.com>
2022-04-28 10:36:49 -0300

    feat: activity navigation

--- app/src/main/java/com/example/motivation/UserActivity.kt
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -1,8 +1,10 @@
 package com.example.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.motivation.databinding.ActivityUserBinding
 
 class UserActivity : AppCompatActivity(), View.OnClickListener {
@@ -12,11 +14,23 @@ class UserActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        binding.buttonSave.setOnClickListener(this)
         supportActionBar?.hide()
     }
 
     override fun onClick(view: View) {
-        TODO("Not yet implemented")
+        when (view.id) {
+            R.id.button_save -> handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val userName = binding.editName.text.toString()
+        if (userName != "") {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            Toast.makeText(this, "O nome deve ser preenchido!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
